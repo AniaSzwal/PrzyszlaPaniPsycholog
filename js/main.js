bf3d922c852a7ae15866795bc6d6666c896a6770
Anna Szwal <aniaszwal@gmail.com>
2023-03-03 16:05:38 +0100

    add main section

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@ const nav = document.querySelector(".nav");
 const navBtn = document.querySelector(".burger-btn");
 const allNavItems = document.querySelectorAll(".nav__item");
 
-//toogle sprawdza, czy dana klasa jest, czy jej nie ma, classList przeszukuje listy klas
+// toogle sprawdza, czy dana klasa jest, czy jej nie ma, classList przeszukuje listy klas
+
+//dla każdegoo elementu w obiekcie talicopodobnym (kadego pojedynczego linku, który jest przechowywany przez 'item' mamy nadać nasluchiwanie na 'clicka' i w momencie, kiedy klik zostanie wyłapany, zostanie usunięta klasa 'nav--active')
+
 const handleNav = () => {
 	nav.classList.toggle("nav--active");
 
-	//dla każdegoo elementu w obiekcie talicopodobnym (kadego pojedynczego linku, który jest przechowywany przez 'item' mamy nadać nasluchiwanie na 'clicka' i w momencie, kiedy klik zostanie wyłapany, zostanie usunięta klasa 'nav--active')
 	allNavItems.forEach(item => {
 		item.addEventListener("click", () => {
 			nav.classList.remove("nav--active");
