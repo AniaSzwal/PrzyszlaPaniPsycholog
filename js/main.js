e21c2e67cf7f53a49fdc7fc85007cf0093917b4c
Anna Szwal <aniaszwal@gmail.com>
2023-03-03 15:33:50 +0100

    add animation-delay in navigation section

--- js/main.js
+++ js/main.js
@@ -6,11 +6,23 @@ const allNavItems = document.querySelectorAll(".nav__item");
 const handleNav = () => {
 	nav.classList.toggle("nav--active");
 
-    //dla każdegoo elementu w obiekcie talicopodobnym (kadego pojedynczego linku, który jest przechowywany przez 'item' mamy nadać nasluchiwanie na 'clicka' i w momencie, kiedy klik zostanie wyłapany, zostanie usunięta klasa 'nav--active')
-	allNavItems.forEach(item => { 
+	//dla każdegoo elementu w obiekcie talicopodobnym (kadego pojedynczego linku, który jest przechowywany przez 'item' mamy nadać nasluchiwanie na 'clicka' i w momencie, kiedy klik zostanie wyłapany, zostanie usunięta klasa 'nav--active')
+	allNavItems.forEach(item => {
 		item.addEventListener("click", () => {
 			nav.classList.remove("nav--active");
 		});
+	});
+
+	handleNavItemsAnimation();
+};
+
+const handleNavItemsAnimation = () => {
+	let delayTime = 0;
+
+	allNavItems.forEach(item => {
+		item.classList.toggle("nav-items-animation");
+		item.style.animationDelay = "0" + "." + delayTime + "s";
+		delayTime++;
 	});
 };
 
