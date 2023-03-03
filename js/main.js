12dff38f07d44eae4b99c00944428e476edb76ea
Anna Szwal <aniaszwal@gmail.com>
2023-03-03 13:33:35 +0100

    add navigation function

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector(".nav");
+const navBtn = document.querySelector(".burger-btn");
+const allNavItems = document.querySelectorAll(".nav__item");
+
+//toogle sprawdza, czy dana klasa jest, czy jej nie ma, classList przeszukuje listy klas
+const handleNav = () => {
+	nav.classList.toggle("nav--active");
+
+    //dla każdegoo elementu w obiekcie talicopodobnym (kadego pojedynczego linku, który jest przechowywany przez 'item' mamy nadać nasluchiwanie na 'clicka' i w momencie, kiedy klik zostanie wyłapany, zostanie usunięta klasa 'nav--active')
+	allNavItems.forEach(item => { 
+		item.addEventListener("click", () => {
+			nav.classList.remove("nav--active");
+		});
+	});
+};
+
+navBtn.addEventListener("click", handleNav);
